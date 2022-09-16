9726d876cb7ca3947bb8cebe49d13a202a8e57de
Mimasss2 <73371588+Mimasss2@users.noreply.github.com>
2022-09-16 09:22:07 +0800

    lab1 finished, passed sanity check.

--- src/cn/edu/hitsz/compiler/lexer/LexicalAnalyzer.java
+++ src/cn/edu/hitsz/compiler/lexer/LexicalAnalyzer.java
@@ -4,6 +4,11 @@ import cn.edu.hitsz.compiler.NotImplementedException;
 import cn.edu.hitsz.compiler.symtab.SymbolTable;
 import cn.edu.hitsz.compiler.utils.FileUtils;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.StreamSupport;
 
 /**
@@ -16,6 +21,8 @@ import java.util.stream.StreamSupport;
  */
 public class LexicalAnalyzer {
     private final SymbolTable symbolTable;
+    private String code;
+    private List<Token> tokens;
 
     public LexicalAnalyzer(SymbolTable symbolTable) {
         this.symbolTable = symbolTable;
@@ -31,7 +38,20 @@ public class LexicalAnalyzer {
         // TODO: 词法分析前的缓冲区实现
         // 可自由实现各类缓冲区
         // 或直接采用完整读入方法
-        throw new NotImplementedException();
+        StringBuilder code = new StringBuilder();
+        try (FileReader reader = new FileReader(path);
+             BufferedReader br = new BufferedReader(reader)
+        ) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                // 一次读入一行数据
+                code.append(line);
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.code = code.toString();
     }
 
     /**
@@ -40,7 +60,77 @@ public class LexicalAnalyzer {
      */
     public void run() {
         // TODO: 自动机实现的词法分析过程
-        throw new NotImplementedException();
+        this.tokens = new ArrayList<>();
+        int state = StateConst.START;
+        int start = 0;
+        int end = 0;
+        int codeLen = this.code.length();
+        char curChar;
+        while (end < codeLen) {
+            curChar = this.code.charAt(end);
+            switch(state) {
+                case(StateConst.START): {
+                    if(curChar == '\r' | Character.isWhitespace(curChar)) {
+                        break;
+                    } else if (Character.isDigit(curChar)) {
+                        state = StateConst.INT_CONST1;
+                        start = end;
+                    } else if (Character.isLetter(curChar)) {
+                        state = StateConst.ID1;
+                        start = end;
+                    } else if (curChar == '=') {
+                        this.tokens.add(Token.simple("="));
+                    } else if (curChar == ',') {
+                        this.tokens.add(Token.simple(","));
+                    } else if (curChar == ';') {
+                        this.tokens.add(Token.simple("Semicolon"));
+                    } else if (curChar == '+') {
+                        this.tokens.add(Token.simple("+"));
+                    } else if (curChar == '-') {
+                        this.tokens.add(Token.simple("-"));
+                    } else if (curChar == '*') {
+                        this.tokens.add(Token.simple("*"));
+                    } else if (curChar == '/') {
+                        this.tokens.add(Token.simple("/"));
+                    } else if (curChar == '(') {
+                        this.tokens.add(Token.simple("("));
+                    } else if (curChar == ')') {
+                        this.tokens.add(Token.simple(")"));
+                    }
+                    break;
+                }
+                case(StateConst.INT_CONST1): {
+                    if(!Character.isDigit(curChar)) {
+                        state = StateConst.START;
+                        String number = this.code.substring(start, end);
+                        this.tokens.add(Token.normal("IntConst", number));
+                        end--;
+                    }
+                    break;
+                }
+                case(StateConst.ID1): {
+                    if(!Character.isLetter(curChar)) {
+                        state = StateConst.START;
+                        String identifierText = this.code.substring(start, end);
+                        end--;
+                        if(TokenKind.isAllowed(identifierText)) {
+                            this.tokens.add(Token.simple(identifierText));
+                        }else {
+                            this.tokens.add(Token.normal("id", identifierText));
+                            if (!symbolTable.has(identifierText)) {
+                                symbolTable.add(identifierText);
+                            }
+                        }
+                    }
+                    break;
+                }
+                default:{
+                }
+            }
+            end++;
+        }
+        this.tokens.add(Token.eof());
+
     }
 
     /**
@@ -53,7 +143,7 @@ public class LexicalAnalyzer {
         // 词法分析过程可以使用 Stream 或 Iterator 实现按需分析
         // 亦可以直接分析完整个文件
         // 总之实现过程能转化为一列表即可
-        throw new NotImplementedException();
+        return this.tokens;
     }
 
     public void dumpTokens(String path) {
---
+++ src/cn/edu/hitsz/compiler/lexer/StateConst.java
@@ -0,0 +1,21 @@
+package cn.edu.hitsz.compiler.lexer;
+
+public class StateConst {
+    public static final int START = 0;
+    public static final int INT_CONST1 = 1;
+    public static final int INT_CONST2 = 2;
+    public static final int ID1 = 3;
+    public static final int ID2 = 4;
+    public static final int EQUAL = 5;
+    public static final int COLON = 6;
+    public static final int SEMICOLON = 7;
+    public static final int ADD = 8;
+    public static final int MINUS = 9;
+    public static final int MUL = 10;
+    public static final int DIV = 11;
+    public static final int LEFT_BRACKET = 12;
+    public static final int RIGHT_BRACKET = 13;
+
+
+
+}
