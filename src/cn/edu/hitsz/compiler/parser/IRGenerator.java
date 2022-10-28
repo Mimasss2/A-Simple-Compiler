6a1f90497b87abead10a47c4c08b72e4ed9084b2
Mimasss2 <73371588+Mimasss2@users.noreply.github.com>
2022-10-28 16:30:08 +0800

    finished lab3

--- src/cn/edu/hitsz/compiler/parser/IRGenerator.java
+++ src/cn/edu/hitsz/compiler/parser/IRGenerator.java
@@ -1,14 +1,20 @@
 package cn.edu.hitsz.compiler.parser;
 
 import cn.edu.hitsz.compiler.NotImplementedException;
+import cn.edu.hitsz.compiler.ir.IRImmediate;
+import cn.edu.hitsz.compiler.ir.IRValue;
+import cn.edu.hitsz.compiler.ir.IRVariable;
 import cn.edu.hitsz.compiler.ir.Instruction;
 import cn.edu.hitsz.compiler.lexer.Token;
 import cn.edu.hitsz.compiler.parser.table.Production;
 import cn.edu.hitsz.compiler.parser.table.Status;
+import cn.edu.hitsz.compiler.parser.table.Term;
 import cn.edu.hitsz.compiler.symtab.SymbolTable;
 import cn.edu.hitsz.compiler.utils.FileUtils;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 // TODO: 实验三: 实现 IR 生成
 
@@ -16,35 +22,134 @@ import java.util.List;
  *
  */
 public class IRGenerator implements ActionObserver {
+    private SymbolTable symbolTable;
+    private List<Instruction> instructions = new ArrayList<>();
+    private Stack<IRValue> irValueStack = new Stack<>();
 
     @Override
     public void whenShift(Status currentStatus, Token currentToken) {
-        // TODO
-        throw new NotImplementedException();
+        switch (currentToken.getKindId()) {
+            case "int", "return", "=", ",", "Semicolon", "+", "-", "*", "/","(", ")" -> {
+                irValueStack.push(null);
+            }
+            case "id" -> {
+                irValueStack.push(IRVariable.named(currentToken.getText()));
+            }
+            case "IntConst" -> {
+                irValueStack.push(IRImmediate.of(Integer.parseInt(currentToken.getText())));
+            }
+            default -> {
+                throw new RuntimeException("Unknown production index");
+            }
+        }
     }
 
     @Override
     public void whenReduce(Status currentStatus, Production production) {
         // TODO
-        throw new NotImplementedException();
+        Instruction instruction;
+        switch (production.index()) {
+            // 我们推荐在 case 后面使用注释标明产生式
+            // 这样能比较清楚地看出产生式索引与产生式的对应关系
+            case 1,2,3,4,5 -> {
+                // P -> S_list;
+                // S_list -> S Semicolon S_list;
+                // S_list -> S Semicolon;
+                // S -> D id;
+                // D -> int;
+                for (int i = 0; i < production.body().size(); i++) {
+                    irValueStack.pop();
+                }
+                irValueStack.push(null);
+            }
+            case 6 -> { // S -> id = E;
+                List<Term> body = production.body();
+                IRValue e = irValueStack.pop();
+                irValueStack.pop();
+                IRValue id = irValueStack.pop();
+                irValueStack.push(null);
+
+                instruction = Instruction.createMov((IRVariable) id, e);
+                instructions.add(instruction);
+            }
+            case 7 -> { // S -> return E;
+                IRValue e = irValueStack.pop();
+                irValueStack.pop();
+                irValueStack.push(null);
+
+                instruction = Instruction.createRet(e);
+                instructions.add(instruction);
+
+            }
+            case 8 -> { // E -> E + A;
+                IRValue a = irValueStack.pop();
+                irValueStack.pop();
+                IRValue e = irValueStack.pop();
+                IRVariable temp = IRVariable.temp();
+                irValueStack.push(temp);
+
+                instruction = Instruction.createAdd(temp, e, a);
+                instructions.add(instruction);
+            }
+            case 9 -> { // E -> E - A;
+                IRValue a = irValueStack.pop();
+                irValueStack.pop();
+                IRValue e = irValueStack.pop();
+                IRVariable temp = IRVariable.temp();
+                irValueStack.push(temp);
+
+                instruction = Instruction.createSub(temp, e, a);
+                instructions.add(instruction);
+            }
+            case 10 -> { // E -> A;
+                // do nothing
+            }
+            case 11 -> { // A -> A * B;
+                IRValue b = irValueStack.pop();
+                irValueStack.pop();
+                IRValue a = irValueStack.pop();
+                IRVariable temp = IRVariable.temp();
+                irValueStack.push(temp);
+
+                instruction = Instruction.createMul(temp, a, b);
+                instructions.add(instruction);
+            }
+            case 12-> { // A -> B;
+                // do nothing
+            }
+            case 13 -> { // B -> ( E );
+                irValueStack.pop();
+                IRValue e = irValueStack.pop();
+                irValueStack.pop();
+                irValueStack.push(e);
+            }
+            case 14,15 -> { // B -> id;   B -> IntConst;
+                IRValue id = irValueStack.pop();
+                irValueStack.push(id);
+            }
+            default -> { //
+                 throw new RuntimeException("Unknown production index");
+                // 或者任何默认行为
+            }
+        }
     }
 
 
     @Override
     public void whenAccept(Status currentStatus) {
         // TODO
-        throw new NotImplementedException();
+        // do nothing
     }
 
     @Override
     public void setSymbolTable(SymbolTable table) {
         // TODO
-        throw new NotImplementedException();
+        this.symbolTable = table;
     }
 
     public List<Instruction> getIR() {
         // TODO
-        throw new NotImplementedException();
+        return this.instructions;
     }
 
     public void dumpIR(String path) {
--- src/cn/edu/hitsz/compiler/parser/SemanticAnalyzer.java
+++ src/cn/edu/hitsz/compiler/parser/SemanticAnalyzer.java
@@ -2,36 +2,88 @@ package cn.edu.hitsz.compiler.parser;
 
 import cn.edu.hitsz.compiler.NotImplementedException;
 import cn.edu.hitsz.compiler.lexer.Token;
+import cn.edu.hitsz.compiler.parser.table.NonTerminal;
 import cn.edu.hitsz.compiler.parser.table.Production;
 import cn.edu.hitsz.compiler.parser.table.Status;
+import cn.edu.hitsz.compiler.parser.table.Term;
+import cn.edu.hitsz.compiler.symtab.SourceCodeType;
 import cn.edu.hitsz.compiler.symtab.SymbolTable;
+import cn.edu.hitsz.compiler.symtab.SymbolTableEntry;
+
+import java.util.List;
+import java.util.Stack;
 
 // TODO: 实验三: 实现语义分析
 public class SemanticAnalyzer implements ActionObserver {
 
+    private SymbolTable symbolTable;
+    private Stack<SourceCodeType> typeStack = new Stack<>();
+    private Stack<Token> tokenStack = new Stack<>();
+
     @Override
     public void whenAccept(Status currentStatus) {
         // TODO: 该过程在遇到 Accept 时要采取的代码动作
-        throw new NotImplementedException();
+        // do nothing
     }
 
     @Override
     public void whenReduce(Status currentStatus, Production production) {
         // TODO: 该过程在遇到 reduce production 时要采取的代码动作
-        throw new NotImplementedException();
+        if (production.index() == 4) {
+//            System.out.println(tokenStack);
+//            System.out.println(typeStack);
+            // S -> D id;
+            Token id = tokenStack.pop();
+            typeStack.pop();
+            tokenStack.pop();
+            SourceCodeType dtype = typeStack.pop();
+
+            typeStack.push(null);
+            tokenStack.push(null);
+
+//            System.out.println(id);
+//            System.out.println(dtype);
+
+            SymbolTableEntry tableEntry = this.symbolTable.get(id.getText());
+            tableEntry.setType(dtype);
+        } else if (production.index() == 5) {
+            // D -> int;
+            SourceCodeType type = typeStack.pop();
+//            System.out.println(type);
+            tokenStack.pop();
+            tokenStack.push(null);
+            typeStack.push(type);
+        } else {
+            int n = production.body().size();
+            while(n > 0) {
+                typeStack.pop();
+                tokenStack.pop();
+                n--;
+            }
+            typeStack.push(null);
+            tokenStack.push(null);
+        }
+
     }
 
     @Override
     public void whenShift(Status currentStatus, Token currentToken) {
         // TODO: 该过程在遇到 shift 时要采取的代码动作
-        throw new NotImplementedException();
+        tokenStack.push(currentToken);
+        if(currentToken.getKind().getIdentifier().equals("int")) {
+//            System.out.println("scanning int");
+            typeStack.push(SourceCodeType.Int);
+        }
+        else {
+            typeStack.push(null);
+        }
     }
 
     @Override
     public void setSymbolTable(SymbolTable table) {
         // TODO: 设计你可能需要的符号表存储结构
         // 如果需要使用符号表的话, 可以将它或者它的一部分信息存起来, 比如使用一个成员变量存储
-        throw new NotImplementedException();
+        this.symbolTable = table;
     }
 }
 
